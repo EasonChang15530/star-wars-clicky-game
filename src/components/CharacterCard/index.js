f60470cdf79e5d44193878240a2ef2552a684a5a
eason <eason15530@gmail.com>
2019-12-22 17:09:02 -0500

    fix card hover bug

--- src/components/CharacterCard/index.js
+++ src/components/CharacterCard/index.js
@@ -5,17 +5,17 @@ function CharacterCard(props) {
   return (
     <div className="col-lg-3 col-md-6 col-sm-12">
       <div className="card">
-      <div className="img-container">
-        <img
-          alt={props.name}
-          src={props.image}
-          onClick={props.randomizeImages} />
-      </div>
-      <div className="content">
-        <strong>
-          {props.name}
-        </strong>
-      </div>
+        <div className="img-container">
+          <img
+            alt={props.name}
+            src={props.image}
+            onClick={props.randomizeImages} />
+        </div>
+        <div className="content">
+          <strong>
+            {props.name}
+          </strong>
+        </div>
       </div>
     </div>
   );
