efa728f2717c2a121b978268e187df6f0c1ec045
eason <eason15530@gmail.com>
2019-12-22 18:06:59 -0500

    Updates

---
+++ precache-manifest.44c07046eea01b856f89c34d5e7d65b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dbcc0fa07dd198c44bfed9b0b295f46",
+    "url": "/star-wars-clicky-game/index.html"
+  },
+  {
+    "revision": "bb3327aa56aa63785011",
+    "url": "/star-wars-clicky-game/static/css/main.53fac7d4.chunk.css"
+  },
+  {
+    "revision": "5917cb7cd014e507d96e",
+    "url": "/star-wars-clicky-game/static/js/2.bdffb9dd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/star-wars-clicky-game/static/js/2.bdffb9dd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb3327aa56aa63785011",
+    "url": "/star-wars-clicky-game/static/js/main.39100871.chunk.js"
+  },
+  {
+    "revision": "571dcab0af50806c2bee",
+    "url": "/star-wars-clicky-game/static/js/runtime-main.aecaaa60.js"
+  }
+]);
